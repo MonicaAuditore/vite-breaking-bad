519d00da4b693f8d515f55de3dc0d247e4ced8f6
MonicaAuditore <mauditore@live.it>
2023-02-06 19:06:30 +0100

    add_modifiche init

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+        @import "./src/styles/partials/reset.scss";
+        @import "./src/styles/partials/mixins.scss";
+        @import "./src/styles/partials/variables.scss";
+        `,
+      },
+    },
+  },
+});
