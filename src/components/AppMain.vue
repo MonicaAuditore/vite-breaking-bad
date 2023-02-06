5a8df1ce4511b9a498ff7362024fc5f1e34b2bf4
MonicaAuditore <mauditore@live.it>
2023-02-06 21:13:12 +0100

    add_style

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -7,19 +7,27 @@ export default {
 <template>
   <div class="bg-warning">
     <div class="container">
-      <div class="text-start">
-        <strong>Found 20 characters</strong>
-      </div>
-
-      <main class="py-3">
-        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-5">
-          <div class="col">Card</div>
-          <div class="col">Card</div>
-          <div class="col">Card</div>
-          <div class="col">Card</div>
-          <div class="col">Card</div>
+      <div class="bg-white p-5">
+        <div class="text-start bg-black text-light py-3 ps-4">
+          <strong>Found 20 characters</strong>
         </div>
-      </main>
+
+        <main class="">
+          <div
+            class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-auto"
+          >
+            <div class="col text-center">
+              <div>
+                <img src="https://via.placeholder.com/300" alt="card image" />
+              </div>
+              <div class="bg-warning p-4">
+                <h3>Character name</h3>
+                <h5>Character status</h5>
+              </div>
+            </div>
+          </div>
+        </main>
+      </div>
     </div>
   </div>
 </template>
